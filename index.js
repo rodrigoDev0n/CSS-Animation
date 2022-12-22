e43e06fc9bad56217fc39e9939f71a493e2d4162
Rodrigo Poblete <pobletedev@gmail.com>
2022-12-22 18:51:17 -0300

    animación typing text

---
+++ index.js
@@ -0,0 +1,26 @@
+const word = document.getElementById('word');
+
+const changeText = () => {
+    setTimeout(() => {
+        word.textContent = "Analista Programador";
+    }, 4000);
+
+    setTimeout(() => {
+        resetAnimation();
+    }, 8000);
+}
+
+const resetAnimation = () => {
+    if (word.textContent === "Analista Programador") {
+        word.textContent = "Frontend Developer";
+        changeText();
+    }
+}
+
+
+changeText();
+
+
+// Rodrigo Poblete
+// Frontend Developer
+// Analista Programador
